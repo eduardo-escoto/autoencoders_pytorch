d21627325fafa488b18eef0462aff5cb96e5ccc5
Eduardo Escoto <me@eduardo.wtf>
2025-10-20 18:04:46 -0700

    just a quick push, will come back to project later

--- ae_pytorch/models/autoencoder.py
+++ ae_pytorch/models/autoencoder.py
@@ -1,3 +1,12 @@
 from torch.nn import Module
 
 
+class Autoencoder(Module):
+    def __init__(self):
+        super().__init__()
+
+    def encode():
+        pass
+
+    def decode():
+        pass
